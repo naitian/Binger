bfdcd57a0b24325d0e34532a42a0599ab45f445c
Naitian Zhou <nzhou2011@gmail.com>
2016-03-05 15:13:32 -0500

    Add Logo, Facebook Auth

--- js/login/login.js
+++ js/login/login.js
@@ -2,7 +2,6 @@ var ref = new Firebase("https://binger.firebaseio.com");
 $(document).ready(function() {
 	console.log("ready");
 	$(".mdl-button--googleplus").click(() => {
-		console.log("hi");
 		ref.authWithOAuthPopup("google", function(error, authData) {
 		  if (error) {
 		    console.log("Login Failed!", error);
@@ -11,4 +10,14 @@ $(document).ready(function() {
 		  }
 		});
 	});
+	$(".mdl-button--facebook").click(() => {
+		ref.authWithOAuthPopup("facebook", function(error, authData) {
+		  if (error) {
+		    console.log("Login Failed!", error);
+		  } else {
+		    console.log("Authenticated successfully with payload:", authData);
+		  }
+		});
+	});
+
 });
