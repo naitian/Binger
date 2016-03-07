58ba8ef877b89caf432d33e1fd3cfcc8010e1dee
Naitian Zhou <nzhou2011@gmail.com>
2016-03-07 14:39:06 -0500

    Revert "Revert "fix facebook login""
    This reverts commit 87e3d4d588054b94e41e2285dbe949cdff9892e7.

--- js/login/login.js
+++ js/login/login.js
@@ -29,7 +29,12 @@ $(document).ready(function() {
 
 function login(auth) {
 	console.log(auth.uid);
-	var uname = auth.google.displayName || auth.facebook.displayName;
+	var uname = "";
+	if(auth.google)
+		uname = auth.google.displayName;
+	else if(auth.facebook)
+		uname = auth.facebook.displayName;
+
 	ref.child(auth.uid).set({
 		name: uname
 	});
